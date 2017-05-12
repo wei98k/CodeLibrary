3d1264d626106ee2f6d8b6e9ec57fb80c6129122
Mr.ma <1012526057@qq.com>
2017-05-12 17:26:08 +0800

    mysql多个库批量备份

---
+++ 服务器相关配置/批量备份数据库_mysql.bat
@@ -0,0 +1,7 @@
+::导出测试成功-TODO是要确认数据导入完整性、可用性 mysqldump 手册 http://www.cnblogs.com/xuejie/archive/2013/01/11/2856911.html
+
+"D:\phpStudy\MySQL\bin\mysqldump.exe" -u用户 -p密码 -h 远程地址 数据库名 > "D:\phpStudy\WWW\test\备份名.sql"
+
+"D:\phpStudy\MySQL\bin\mysqldump.exe" -u用户 -p密码 -h 远程地址 数据库名 > "D:\phpStudy\WWW\test\备份名.sql"
+
+"D:\phpStudy\MySQL\bin\mysqldump.exe" -u用户 -p密码 -h 远程地址 数据库名 > "D:\phpStudy\WWW\test\备份名.sql"
